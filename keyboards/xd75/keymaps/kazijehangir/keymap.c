384342d6665d4ef5856c24b7375b7bc72499d4a6
Jehangir Kazi <kazijehagir@gmail.com>
2021-10-22 10:37:40 -0700

    Add media keys

--- keyboards/xd75/keymaps/kazijehangir/keymap.c
+++ keyboards/xd75/keymaps/kazijehangir/keymap.c
@@ -38,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
-    KC_F8,          KC_1,    KC_2,    KC_3,    KC_4,           KC_5,            KC_PGUP, KC_SPC,  KC_PGDN,     KC_6,           KC_7,             KC_8,     KC_9,    KC_0,    KC_F9, \
-    KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,           KC_T,            KC_LBRC, KC_BSLS, KC_RBRC,     KC_Y,           KC_U,             KC_I,     KC_O,    KC_P,    KC_QUOT, \
-    LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,           KC_G,            KC_MINS, KC_GRV,  KC_EQL,      KC_H,           KC_J,             KC_K,     KC_L,    KC_SCLN, RCTL_T(KC_ENT),  \
-    KC_LSPO,        KC_Z,    KC_X,    KC_C,    KC_V,           KC_B,            KC_HOME, KC_UP,   KC_END,      KC_N,           KC_M,             KC_COMM,  KC_DOT,  KC_SLSH, KC_RSPC, \
-    KC_HYPR,        KC_LGUI, KC_LALT, MO(_FN), LALT_T(KC_DEL), LGUI_T(KC_SPC),  KC_LEFT, KC_DOWN, KC_RGHT,     RGUI_T(KC_SPC), LALT_T(KC_BSPC),  MO(_LED), KC_RALT, KC_RGUI, RCTL_T(KC_ESC)  \
+    KC_F8,          KC_1,    KC_2,    KC_3,    KC_4,           KC_5,            KC_PGUP, KC_SPC,  KC_PGDN,     KC_6,           KC_7,             KC_8,     KC_9,     KC_0,     KC_F9, \
+    KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,           KC_T,            KC_LBRC, KC_BSLS, KC_RBRC,     KC_Y,           KC_U,             KC_I,     KC_O,     KC_P,     KC_QUOT, \
+    LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,           KC_G,            KC_MINS, KC_GRV,  KC_EQL,      KC_H,           KC_J,             KC_K,     KC_L,     KC_SCLN,  RCTL_T(KC_ENT),  \
+    KC_LSPO,        KC_Z,    KC_X,    KC_C,    KC_V,           KC_B,            KC_HOME, KC_UP,   KC_END,      KC_N,           KC_M,             KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSPC, \
+    KC_HYPR,        MO(_FN), MO(_FN), MO(_FN), LALT_T(KC_DEL), LGUI_T(KC_SPC),  KC_LEFT, KC_DOWN, KC_RGHT,     RGUI_T(KC_SPC), LALT_T(KC_BSPC),  MO(_LED), MO(_LED), MO(_LED), KC_HYPR  \
  ),
 /* NAV FUNCTIONALITY
  * .--------------------------------------------------------------------------------------------------------------------------------------.
@@ -59,11 +59,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_FN] = LAYOUT_ortho_5x15( /* FUNCTION */
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
+    KC_F1,   KC_F2  ,   KC_F3   , KC_F4     , KC_F5     , KC_F6  , KC_NLCK, KC_SLSH, KC_ASTR, KC_F7  ,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
     _______, _______, KC_MS_BTN1, KC_MS_UP  , KC_MS_BTN2, _______, _______, _______, _______, _______, _______, KC_UP  , _______,  _______, _______, \
     _______, _______, KC_MS_LEFT, KC_MS_DOWN,KC_MS_RIGHT, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, \
-    _______, _______, _______   , _______   , _______   , _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, \
-    _______, _______, _______   , _______   , _______   , _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______ \
+    _______, _______, _______   , _______   , _______   , _______, _______, KC_VOLU, _______, _______, _______, _______, _______,  _______, _______, \
+    _______, _______, _______   , _______   , _______   , KC_MPLY, KC_MPRV, KC_VOLD, KC_MNXT, KC_MPLY, _______, _______, _______,  _______, _______ \
  ),
 
 /* LED FUNCTIONALITY
